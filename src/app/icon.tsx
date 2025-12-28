e6e268c2934bc5fce1d22df141febcc0bea12739
Het Patel <het.patel@klearcom.com>
2025-12-28 12:18:53 +0530

    implement logo

---
+++ src/app/icon.tsx
@@ -0,0 +1,50 @@
+import { ImageResponse } from 'next/og'
+
+// Image metadata
+export const size = {
+    width: 32,
+    height: 32,
+}
+export const contentType = 'image/png'
+
+// Image generation
+export default function Icon() {
+    return new ImageResponse(
+        (
+            // ImageResponse JSX element
+            <div
+                style={{
+                    fontSize: 24,
+                    background: 'linear-gradient(to right, #9333ea, #db2777)',
+                    width: '100%',
+                    height: '100%',
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    color: 'white',
+                    borderRadius: '20%',
+                }}
+            >
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="20"
+                    height="20"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                >
+                    <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4" />
+                    <polyline points="7 10 12 15 17 10" />
+                    <line x1="12" x2="12" y1="15" y2="3" />
+                </svg>
+            </div>
+        ),
+        // ImageResponse options
+        {
+            ...size,
+        }
+    )
+}
